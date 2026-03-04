e62b18f956900d74cb94db115311eb540f038f5d
samitnayak16-code <samitnayak16@gmail.com>
2026-03-04 00:09:29 -0800

    Update autons.h

--- include/autons.h
+++ include/autons.h
@@ -11,6 +11,12 @@ void drive_test();
 void turn_test();
 void swing_test();
 void full_test();
+
+void RightSide4_Push();
+void LeftSide4_Push();
+void RightSide7_Push();
+void LeftSide7_Push();
+void LeftSide4_3();
 void odom_test();
 void tank_odom_test();
 void holonomic_odom_test();
